34b5f45e5b8b0dcd1367581e37687f2a74d3f6ee
Nikita Velgin <direct.vegas@gmail.com>
2019-06-28 17:21:06 +0300

    Bump version of kind

--- homeworks/kubernetes-intro/run.sh
+++ homeworks/kubernetes-intro/run.sh
@@ -8,7 +8,7 @@ chmod +x kubectl
 sudo mv kubectl /usr/local/bin/
 
 # Download kind
-curl -Lo kind https://github.com/kubernetes-sigs/kind/releases/download/v0.3.0/kind-linux-amd64
+curl -Lo kind https://github.com/kubernetes-sigs/kind/releases/download/v0.4.0/kind-linux-amd64
 chmod +x kind 
 sudo mv kind /usr/local/bin/
 
