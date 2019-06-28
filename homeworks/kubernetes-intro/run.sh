7ef310abb1227e4d6438bb8979b2eb2e4f3a0f5c
Nikita Velgin <direct.vegas@gmail.com>
2019-06-28 18:33:57 +0300

    Set

--- homeworks/kubernetes-intro/run.sh
+++ homeworks/kubernetes-intro/run.sh
@@ -1,6 +1,5 @@
-
 #!/bin/bash
-set -e
+set -xe
 
 # Download kubectl
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl 
