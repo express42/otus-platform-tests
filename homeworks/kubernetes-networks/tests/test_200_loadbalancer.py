858451ffd5b0d6ee1dffd5246bf45401907b82af
Alexander Suleymanov <asu@express42.com>
2019-07-29 19:28:08 +0300

    k8s.networks.hw || Tests for Web LoadBalancer service

--- homeworks/kubernetes-networks/tests/test_200_loadbalancer.py
+++ homeworks/kubernetes-networks/tests/test_200_loadbalancer.py
@@ -1,5 +1,7 @@
 import pytest
-
+import kubetest.objects
+import ipaddress
+from time import sleep
 
 """
 Fixture definitions. Common fixtures, such as toolbox pod are in conftest.py
@@ -7,15 +9,16 @@ Fixture definitions. Common fixtures, such as toolbox pod are in conftest.py
 
 
 @pytest.fixture(scope="module")
-def web_service_lb(request, kube_module):
+def web_service_lb(kube_module, web_deploy) -> kubetest.objects.Service:
     # Wait for Service to be ready on the cluster
     sm = kube_module.load_service("./kubernetes-networks/web-svc-lb.yaml")
     sm.create()
-    kube_module.wait_for_registered(timeout=30)
+    kube_module.wait_until_created(sm, timeout=10)
     services = kube_module.get_services()
     s = services.get("web-svc-lb")
-    s.wait_until_ready(timeout=30)
     yield s
+    s.delete(options=None)
+    s.wait_until_deleted()
 
 
 """
@@ -24,20 +27,51 @@ Actual tests code below. Fixtures are called and created as needed
 
 
 @pytest.mark.it("TEST: Check LoadBalancer and MetalLB configurations")
-def test_resource_existance(web_deploy, web_service_lb):
-    assert web_deploy is not None, "Deployment does not exist"
+def test_svc_lb_resource_existance(web_service_lb):
     assert web_service_lb is not None, "LoadBalancer Service for Web does not exist"
 
 
 @pytest.mark.it("Verify Web LoadBalancer service configuration")
 def test_service_lb(web_service_lb):
-    assert web_service_lb.is_ready() is True, "Service is not ready (endpoints failing)"
+    web_service_lb.wait_until_ready(timeout=30)
 
-    assert web_service_lb.obj.spec.type == "LoadBalancer"
-    assert web_service_lb.obj.spec.ports[0].port == 80
+    assert web_service_lb.is_ready() is True, "Service is not ready (endpoints failing)"
+    assert (
+        web_service_lb.obj.spec.type == "LoadBalancer"
+    ), "Service type is not LoadBalancer"
+    assert web_service_lb.obj.spec.ports[0].port == 80, "Service port is not 80"
 
 
 @pytest.mark.it("wev-svc-lb Service should have 3 healthy endpoints")
 def test_lb_service_endpoints(web_service_lb):
     ep = web_service_lb.get_endpoints()
     assert len(ep[0].subsets[0].addresses) == 3
+
+
+@pytest.mark.it("Check LoadBalancer ingress IP address")
+def test_lb_service_ingress(web_service_lb):
+    sleep(3)
+    web_service_lb.refresh()
+    lb_ingress = web_service_lb.obj.status.load_balancer.ingress
+    assert lb_ingress is not None, "LoadBalancer ingress endpoint is not set"
+    assert lb_ingress[0].ip is not None, "LoadBalancer ingress IP is not defined"
+    assert (
+        ipaddress.ip_address(lb_ingress[0].ip)
+        in ipaddress.ip_network("172.17.255.0/24").hosts()
+    ), "Assigned LB ingress IP is not from 172.17.255.0/24 range"
+
+
+@pytest.mark.it("Verify external connectivity for web-svc-lb service")
+def test_lb_external_connection(web_service_lb, test_container):
+    ip = web_service_lb.obj.status.load_balancer.ingress[0].ip
+    assert ip is not None
+    res = list()
+    for test in range(0, 4):
+        res.append(
+            test_container.check_output(
+                "wget -T 10 -O- http://{}:{}/index.html | grep HOSTNAME".format(
+                    ip, "80"
+                )
+            )
+        )
+    assert len(set(res)) > 1, "Requests are not balanced between pods"
