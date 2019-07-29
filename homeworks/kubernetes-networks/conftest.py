40b29b6c37e474ed7a879d504319874ac752400d
Alexander Suleymanov <asu@express42.com>
2019-07-29 19:27:00 +0300

    k8s.networks.hw || Small cleanups and type annotations in common fixtures, MetalLB fixture added

--- homeworks/kubernetes-networks/conftest.py
+++ homeworks/kubernetes-networks/conftest.py
@@ -1,7 +1,10 @@
-import kubetest.plugin as kbp
+import kubetest.objects
+import kubetest.plugin
+from kubernetes import client, config, utils
 import pytest
 import subprocess
 import testinfra
+import yaml
 
 
 """
@@ -9,8 +12,23 @@ Reusable fixtures are defined there, plus everything you'd like to do with pytes
 """
 
 
+@pytest.fixture(scope="session")
+def metallb(request):
+    config.load_kube_config()
+    k8s_client = client.ApiClient()
+    utils.create_from_yaml(k8s_client, "./manifests/metallb.yaml")
+    utils.create_from_yaml(k8s_client, "./kubernetes-networks/metallb-config.yaml")
+
+    def fin():
+        # Someday i'll do it better ))
+        print("Calling Kubectl to delete objects from MetalLB manifest")
+        subprocess.check_call(["kubectl", "delete", "-f", "./manifests/metallb.yaml"])
+
+    request.addfinalizer(fin)
+
+
 @pytest.fixture(scope="function")
-def test_container(request):
+def test_container(request) -> testinfra.host.Host:
     # run a container
     docker_id = (
         subprocess.check_output(
@@ -28,17 +46,19 @@ def test_container(request):
         .decode()
         .strip()
     )
-    # return a testinfra connection to the container
+
     yield testinfra.get_host("docker://" + docker_id)
+    # return a testinfra connection to the container
     subprocess.check_call(["docker", "rm", "-f", docker_id])
 
 
 @pytest.fixture(scope="function")
-def test_pod(request, kube_module):
+def test_pod(kube_module) -> testinfra.host.Host:
     p = kube_module.load_pod("./manifests/test-pod.yaml")
     p.create()
-    kube_module.wait_for_registered(timeout=30)
+    kube_module.wait_until_created(p, timeout=10)
     p.wait_until_ready(timeout=30)
+
     yield testinfra.get_host(
         "kubectl://{pod}?namespace={ns}".format(ns=p.namespace, pod=p.name)
     )
@@ -46,19 +66,15 @@ def test_pod(request, kube_module):
 
 
 @pytest.fixture(scope="module")
-def web_deploy(request, kube_module):
+def web_deploy(kube_module) -> kubetest.objects.Deployment:
     # wait for the manifests loaded by the 'applymanifests' marker
     # to be ready on the cluster
     dm = kube_module.load_deployment("./kubernetes-networks/web-deploy.yaml")
     dm.create()
-    kube_module.wait_for_registered(timeout=30)
+    kube_module.wait_until_created(dm, timeout=10)
     deployments = kube_module.get_deployments()
     d = deployments.get("web")
     d.wait_until_ready(timeout=60)
     yield d
-    """
-    This really hacky and weird way of cleaning up test namespaces
-    It should be fixed with patching kubetest code, but I still haven't
-    found, where things gone wrong.
-    """
-    kbp.pytest_keyboard_interrupt()
+    d.delete(options=None)
+    d.wait_until_deleted()
