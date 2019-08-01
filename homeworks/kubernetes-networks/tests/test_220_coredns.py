7a09691480c90cda9d4f4e8c023e4765e2a5a3ee
Alexander Suleymanov <asu@express42.com>
2019-08-01 16:54:18 +0300

    k8s.networks.hw || Cosmetics in coredns code, just under the hood changes

--- homeworks/kubernetes-networks/tests/test_220_coredns.py
+++ homeworks/kubernetes-networks/tests/test_220_coredns.py
@@ -1,25 +1,29 @@
-import pytest
-import kubetest.objects
-from kubernetes.client import models
 from time import sleep
-import yaml
+from typing import Dict, List, Optional
 import os
+import yaml
+from kubernetes.client import models
 from kubetest.manifest import get_type, new_object
+import kubetest.objects
+import pytest
+import testinfra.host
 
 TEST_QUERY = "kubernetes.default.svc.cluster.local"
 
 """
 these functions are copy-pasted shamelessly from kubetest.manifest.
 The only difference - load_file **ignores** unknown manifest types
+'Raises' is simply replaced with 'Print' call. That's all.
 """
 
 
-def load_file(path):
-    with open(path, "r") as f:
-        manifests = yaml.load_all(f, Loader=yaml.SafeLoader)
-        objs = []
+def load_file(path: str) -> List[object]:
+    """ Loads manifest from YAML file and returns kubetest ApiObjects"""
+    with open(path, "r") as input_file:
+        manifests = yaml.load_all(input_file, Loader=yaml.SafeLoader)
+        objs: List[object] = list()
         for manifest in manifests:
-            obj_type = get_type(manifest)
+            obj_type: Optional[object] = get_type(manifest)
             if obj_type is None:
                 print(
                     "Unable to determine object type for manifest: {}".format(manifest)
@@ -29,50 +33,54 @@ def load_file(path):
     return objs
 
 
-def load_path(path):
+def load_path(path: str) -> List[object]:
+    """ Calls load_file for all YAML files in the specified directory """
     if not os.path.isdir(path):
         raise ValueError("{} is not a directory".format(path))
 
-    objs = []
-    for f in os.listdir(path):
-        if os.path.splitext(f)[1].lower() in [".yaml", ".yml"]:
-            objs = objs + load_file(os.path.join(path, f))
+    objs: List[object] = list()
+    for file_name in os.listdir(path):
+        if os.path.splitext(file_name)[1].lower() in [".yaml", ".yml"]:
+            objs = objs + load_file(os.path.join(path, file_name))
     return objs
 
 
-"""
-END
-"""
+# Kubetest copy-paste END
 
 
-def preload_manifests():
+def preload_manifests(path: str, selector: Dict[str, str]) -> List[models.V1Service]:
+    """
+    Loads all manifest files from dir, and returns a list of all
+    Service objects with matching selector in spec
+    """
     try:
-        objects = load_path("./kubernetes-networks")
+        objects = load_path(path)
     except ValueError:
         pass
 
     coredns_svc = [
         obj
         for obj in objects
-        if isinstance(obj, models.V1Service)
-        and obj.spec.selector == {"k8s-app": "kube-dns"}
+        if isinstance(obj, models.V1Service) and obj.spec.selector == selector
     ]
     return coredns_svc
 
 
-manifests = preload_manifests()
+MANIFESTS = preload_manifests(
+    path="./kubernetes-networks", selector={"k8s-app": "kube-dns"}
+)
 
-if len(manifests) == 0:
+if not MANIFESTS:
     pytest.skip("Skipping CoreDNS tests", allow_module_level=True)
 
 
-def get_services_by_proto(proto_string=None) -> [models.V1Service]:
-    svc_list = [obj for obj in manifests if obj.spec.ports[0].protocol == proto_string]
-    return svc_list
+def get_services_by_proto(proto_string: str) -> List[models.V1Service]:
+    return [obj for obj in MANIFESTS if obj.spec.ports[0].protocol == proto_string]
 
 
 @pytest.fixture(scope="module")
-def coredns_tcp(kube_module) -> kubetest.objects.Service:
+def coredns_tcp(kube_module: kubetest.client.TestClient) -> kubetest.objects.Service:
+    """ Return instance of CoreDNS LoadBalancer service for TCP endpoint"""
     svc_list = get_services_by_proto("TCP")
     svc = kubetest.objects.Service(svc_list[0])
     svc.create()
@@ -84,7 +92,8 @@ def coredns_tcp(kube_module) -> kubetest.objects.Service:
 
 
 @pytest.fixture(scope="module")
-def coredns_udp(kube_module) -> kubetest.objects.Service:
+def coredns_udp(kube_module: kubetest.client.TestClient) -> kubetest.objects.Service:
+    """ Return instance of CoreDNS LoadBalancer service for UDP endpoint"""
     svc_list = get_services_by_proto("UDP")
     svc = kubetest.objects.Service(svc_list[0])
     svc.create()
@@ -95,9 +104,10 @@ def coredns_udp(kube_module) -> kubetest.objects.Service:
     svc.wait_until_deleted()
 
 
-def get_lb_ip(service=None):
-    ip = None
-    count = 0
+def get_lb_ip(service: kubetest.objects.Service) -> Optional[str]:
+    """ Returns IP address of LoadBalancer-assigned Ingress or None """
+    ip: Optional[str] = None
+    count: int = 0
     while ip is None:
         if count == 3:
             break
@@ -113,32 +123,17 @@ def get_lb_ip(service=None):
 
 
 @pytest.mark.xfail(
-    len(manifests) < 2,
+    len(MANIFESTS) < 2,
     run=False,
     reason="TCP or UDP service for CoreDNS is not defined",
 )
 @pytest.mark.it("CoreDNS services MUST have the same LB ingress IP")
-def test_coredns_shared_ip(coredns_tcp, coredns_udp):
+def test_coredns_shared_ip(
+    coredns_tcp: kubetest.objects.Service, coredns_udp: kubetest.objects.Service
+) -> None:
 
-    tcp_ip = get_lb_ip(coredns_tcp)
-    udp_ip = get_lb_ip(coredns_udp)
-
-    # tcp_ip, udp_ip = None, None
-    # count = 0
-    # while udp_ip is None or tcp_ip is None:
-    #     if count == 3:
-    #         break
-    #     try:
-    #         coredns_udp.refresh()
-    #         coredns_tcp.refresh()
-
-    #         udp_ip = coredns_udp.obj.status.load_balancer.ingress[0].ip
-    #         tcp_ip = coredns_tcp.obj.status.load_balancer.ingress[0].ip
-    #     except TypeError:
-    #         print("Still have no IP. Retrying...")
-    #         sleep(3)
-    #         count = count + 1
-    #         continue
+    tcp_ip: Optional[str] = get_lb_ip(coredns_tcp)
+    udp_ip: Optional[str] = get_lb_ip(coredns_udp)
 
     assert udp_ip == tcp_ip, "UDP ({}) and TCP({}) LB IPs don't match".format(
         udp_ip, tcp_ip
@@ -151,17 +146,18 @@ def test_coredns_shared_ip(coredns_tcp, coredns_udp):
     reason="TCP service for CoreDNS is not defined",
 )
 @pytest.mark.it("Verify CoreDNS via MetalLB. Using TCP")
-def test_coredns_tcp(coredns_tcp, test_container):
-    assert coredns_tcp is not None and isinstance(
+def test_coredns_tcp(
+    coredns_tcp: kubetest.objects.Service, test_container: testinfra.host.Host
+) -> None:
+    assert isinstance(
         coredns_tcp, kubetest.objects.Service
     ), "CoreDNS Service for TCP Endpoint is not available"
 
     assert (
-        coredns_tcp.obj.status.load_balancer is not dict()
+        coredns_tcp.obj.status.load_balancer != {}
     ), "LoadBalancer ingress status is not set. Is MetalLB available?"
 
-    ip = get_lb_ip(coredns_tcp)
-
+    ip: Optional[str] = get_lb_ip(coredns_tcp)
     assert ip is not None, "No IP address assigned for TCP endpoint"
 
     test_container.run("apk --no-cache -q add drill")
@@ -169,7 +165,7 @@ def test_coredns_tcp(coredns_tcp, test_container):
         test_container.package("drill").is_installed is True
     ), "Can't bootstrap test container - curl is not installed"
 
-    tcp = test_container.check_output(
+    tcp: str = test_container.check_output(
         "drill -t {rec} @{ip}".format(ip=ip, rec=TEST_QUERY)
     )
     assert (
@@ -184,17 +180,19 @@ def test_coredns_tcp(coredns_tcp, test_container):
     reason="UDP service for CoreDNS is not defined",
 )
 @pytest.mark.it("Verify CoreDNS via MetalLB. Using UDP")
-def test_coredns_udp(coredns_udp, test_container):
+def test_coredns_udp(
+    coredns_udp: kubetest.objects.Service, test_container: testinfra.host.Host
+) -> None:
 
     assert coredns_udp is not None and isinstance(
         coredns_udp, kubetest.objects.Service
     ), "CoreDNS Service for UDP Endpoint is not available"
 
     assert (
-        coredns_udp.obj.status.load_balancer.ingress is not dict()
+        coredns_udp.obj.status.load_balancer.ingress != {}
     ), "LoadBalancer ingress status is not set. Is MetalLB available?"
 
-    ip = get_lb_ip(coredns_udp)
+    ip: Optional[str] = get_lb_ip(coredns_udp)
     assert ip is not None, "No IP address assigned for UDP endpoint"
 
     test_container.run("apk --no-cache -q add drill")
@@ -202,7 +200,7 @@ def test_coredns_udp(coredns_udp, test_container):
         test_container.package("drill").is_installed is True
     ), "Can't bootstrap test container - curl is not installed"
 
-    udp = test_container.check_output(
+    udp: str = test_container.check_output(
         "drill -u {rec} @{ip}".format(ip=ip, rec=TEST_QUERY)
     )
 
