878db0ea84361d41efb919b17cdcb196e4fb4006
Sergii Sinienok <sergii.sinienok@s-sinienok.local>
2019-08-12 18:33:14 +0300

    Local Test run script: fixes for the shellcheck+shfmt. Fixes for issues in PR comments

--- local-run.sh
+++ local-run.sh
@@ -1,37 +1,38 @@
-
-printf "Howdy! I will run [otus-platform-tests] locally for you.\n"
-
-dirtocheck=""
+#!/bin/bash
+echo "Howdy! I will run [otus-platform-tests] locally for you."
+echo
 platformdir=""
 hwdir=""
 
 if [[ ! -d $1 || "$2" == "" ]]; then
-    printf "You'll supposed to point me in, I'll need to know where to start.\n"
+    echo "You'll supposed to point me in, I'll need to know where to start."
 
     read -p "Your local platform folder (something like $HOME/src/{gitname}_platform):" platformdir
 
-    if [ ! -d $platformdir ]; then
-        printf "Does not look like a valid folder. You can do better!"
+    if [ ! -d "$platformdir" ]; then
+        echo "Does not look like a valid folder. You can do better!"
         exit 1
     fi
 
     read -p "Homework dir you want me to check (kubernetes-intro or others):" hwdir
 
     if [ "$hwdir" == "" ]; then
-        printf "No in - no out! Point me somewhere to do the job."
+        echo "No in - no out! Point me somewhere to do the job."
         exit 1
     fi
-
-    dirtocheck="$platformdir/$hwdir"
 else
     platformdir="$1"
     hwdir="$2"
-    dirtocheck="$platformdir/$hwdir"
 fi
 
-printf "\nYou pointed me here [$platformdir] to check [$hwdir]. I will obey! \n"
-printf "\n"
-cd $platformdir
-export TRAVIS_BRANCH=$(echo $hwdir | tr -d '\r')
+printf "\nYou pointed me here [%s] to check [%s]. I will obey! \n" "$platformdir" "$hwdir"
+echo
 
-curl https://raw.githubusercontent.com/express42/otus-platform-tests/2019-06/run.sh | bash
+pathtorunsh="$PWD/run.sh"
+
+cd "$platformdir" || exit 1
+
+TRAVIS_BRANCH=$(echo "$hwdir" | tr -d '\r')
+export TRAVIS_BRANCH
+
+bash "$pathtorunsh"
