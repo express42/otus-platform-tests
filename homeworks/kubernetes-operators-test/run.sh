695897e1deef6cdb59da2589bc17cada9f59f7ef
Evgeny Nikitin <evgenikk96@gmail.com>
2019-09-25 19:01:13 +0300

    add wait timeout

--- homeworks/kubernetes-operators-test/run.sh
+++ homeworks/kubernetes-operators-test/run.sh
@@ -28,7 +28,7 @@ kubectl apply -f kubernetes-operators/deploy/deploy-operator.yml
 kubectl apply -f kubernetes-operators/deploy/cr.yml
 
 
-kubectl wait --for=condition=Available deployment/mysql-operator
+kubectl wait --for=condition=Available deployment/mysql-operator --timeout=300s
 export MYSQLPOD="$(kubectl get pods -l app=mysql-instance -o jsonpath="{.items[*].metadata.name}")"
 kubectl wait --for=condition=Ready pod/$MYSQLPOD --timeout=300s
 
