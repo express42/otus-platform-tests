285d3d0525512575b0d633fba174f1db8664cfc5
Evgeny Nikitin <evgenikk96@gmail.com>
2019-09-26 17:17:36 +0300

    fix mysql command

--- homeworks/kubernetes-operators-test/run.sh
+++ homeworks/kubernetes-operators-test/run.sh
@@ -55,9 +55,9 @@ kubectl wait --for=condition=Ready pod/$MYSQLPOD --timeout=300s
 
 # Fill DB:
 
-kubectl exec -it $MYSQLPOD -- mysql -u root -potuspassword mysql  -e "CREATE TABLE test ( id smallint unsigned not null auto_increment, name varchar(20) not null, constraint pk_example primary key (id) );" otus-database
-kubectl exec -it $MYSQLPOD -- mysql  -e -u root -potuspassword mysql  "INSERT INTO test ( id, name ) VALUES ( null, 'some data' );" otus-database
-kubectl exec -it $MYSQLPOD -- mysql  -e -u root -potuspassword mysql  "INSERT INTO test ( id, name ) VALUES ( null, 'some data-2' );" otus-database
+kubectl exec -it $MYSQLPOD -- mysql  -potuspassword -e "CREATE TABLE test ( id smallint unsigned not null auto_increment, name varchar(20) not null, constraint pk_example primary key (id) );" otus-database
+kubectl exec -it $MYSQLPOD -- mysql  -potuspassword -e "INSERT INTO test ( id, name ) VALUES ( null, 'some data' );" otus-database
+kubectl exec -it $MYSQLPOD -- mysql  -potuspassword -e  "INSERT INTO test ( id, name ) VALUES ( null, 'some data-2' );" otus-database
 
 # Redeploy mysql
 kubectl delete -f kubernetes-operators/deploy/cr.yml
