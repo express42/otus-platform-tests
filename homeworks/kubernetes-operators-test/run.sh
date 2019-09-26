bdf385fd3ff55aaa39d0c1dcd6220650766d868d
Evgeny Nikitin <evgenikk96@gmail.com>
2019-09-26 15:40:46 +0300

    fix tests

--- homeworks/kubernetes-operators-test/run.sh
+++ homeworks/kubernetes-operators-test/run.sh
@@ -41,7 +41,8 @@ kubectl exec -it $MYSQLPOD -- mysql -potuspassword -e "INSERT INTO test ( id, na
 
 # Redeploy mysql
 kubectl delete -f kubernetes-operators/deploy/cr.yml
-sleep 3
+sleep 10
+kubectl wait --for=condition=complete jobs/backup-mysql-instance-job
 kubectl apply -f kubernetes-operators/deploy/cr.yml
 sleep 10
 kubectl wait --for=condition=complete jobs/restore-mysql-instance-job  --timeout=600s
