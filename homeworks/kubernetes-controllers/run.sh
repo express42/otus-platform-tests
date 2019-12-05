45fa8eeb0d4eebfefd12a7e2fc996f978fca5a29
Nikita Velgin <direct.vegas@gmail.com>
2019-12-05 14:57:47 +0300

    Fuck ubuntu, fuck Go

--- homeworks/kubernetes-controllers/run.sh
+++ homeworks/kubernetes-controllers/run.sh
@@ -20,13 +20,15 @@ download(){
 
 go_bootstrap() {
     # Install latest Go version
-    sudo apt-get update
-    sudo apt-get -y upgrade
     wget https://dl.google.com/go/go1.13.4.linux-amd64.tar.gz
-    sudo tar -xvf go1.13.4.linux-amd64.tar.gz
+    sudo tar -xf go1.13.4.linux-amd64.tar.gz
     sudo mv go /usr/local
-    cp -fr ./otus-platform-tests/homeworks/${HOMEWORK}/* ./
+    export GOROOT=/usr/local/go
+    export GOPATH=$HOME/
+    export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
     go version
+    
+    cp -fr ./otus-platform-tests/homeworks/${HOMEWORK}/* ./
 }
 
 prepare() {
