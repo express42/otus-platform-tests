b222ac65005664c5dfbd9e9da7bea849d2bff4e0
Nikita Velgin <direct.vegas@gmail.com>
2019-12-05 14:37:52 +0300

    change go installation method

--- homeworks/kubernetes-controllers/run.sh
+++ homeworks/kubernetes-controllers/run.sh
@@ -20,9 +20,11 @@ download(){
 
 go_bootstrap() {
     # Install latest Go version
-    sudo add-apt-repository ppa:longsleep/golang-backports
     sudo apt-get update
-    sudo apt-get install golang-go
+    sudo apt-get -y upgrade
+    wget https://dl.google.com/go/go1.13.4.linux-amd64.tar.gz
+    sudo tar -xvf go1.13.3.linux-amd64.tar.gz
+    sudo mv go /usr/local
     cp -fr ./otus-platform-tests/homeworks/${HOMEWORK}/* ./
     go version
 }
