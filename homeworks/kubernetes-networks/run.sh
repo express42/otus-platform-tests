87553e496e88f9e29c0432a47e36727578e6a49c
Alexander Suleymanov <asu@express42.com>
2020-05-22 08:13:18 +0300

    new ingress-nginx and metallb versions with some dirty hacks

--- homeworks/kubernetes-networks/run.sh
+++ homeworks/kubernetes-networks/run.sh
@@ -3,7 +3,7 @@ set -xe
 
 export TERM=xterm
 
-download(){
+download() {
     export KUBECTL_VER="$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)"
     export KIND_VER="v0.4.0"
 
@@ -24,8 +24,8 @@ pytest_bootstrap() {
     sudo apt-get -q -y install python3-venv
 
     # Create and activate venv for PyTest
-    python3 -m venv /tmp/pytest && \
-    source /tmp/pytest/bin/activate
+    python3 -m venv /tmp/pytest &&
+        source /tmp/pytest/bin/activate
 
     pip3 install -q -U pip setuptools
 
@@ -40,9 +40,10 @@ prepare() {
     export KUBECONFIG="$(kind get kubeconfig-path)"
     # Wait while all components in kube-system namespace will start
     kubectl wait --for=condition=Ready pod --all -n kube-system --timeout=300s
-
-    curl -L -o ./manifests/metallb.yaml https://raw.githubusercontent.com/google/metallb/v0.8.1/manifests/metallb.yaml
-    curl -L -o ./manifests/ingress-nginx.yaml https://raw.githubusercontent.com/kubernetes/ingress-nginx/nginx-0.30.0/deploy/static/mandatory.yaml
+    kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
+    kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+    curl -L -o ./manifests/metallb.yaml https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
+    curl -L -o ./manifests/ingress-nginx.yaml https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/cloud/deploy.yaml
 }
 
 run_tests() {
