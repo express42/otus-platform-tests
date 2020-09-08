1987c37451e86b6a8e298de3e5c56d81cacc6885
mrgreyves <mrgreyves@gmail.com>
2020-09-08 14:23:11 +0300

    change kubectl version to 1.18.6

--- homeworks/kubernetes-storage/run.sh
+++ homeworks/kubernetes-storage/run.sh
@@ -2,7 +2,7 @@
 set -xe
 
 # Download kubectl
-curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.17.11/bin/linux/amd64/kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.18.6/bin/linux/amd64/kubectl
 chmod +x kubectl
 sudo mv kubectl /usr/local/bin/
 
