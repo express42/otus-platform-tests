11c3218627b9c64b4af91572c3cd66375afbaf07
mrgreyves <mrgreyves@gmail.com>
2020-09-23 20:46:44 +0300

    try fix network homework

--- homeworks/kubernetes-networks/run.sh
+++ homeworks/kubernetes-networks/run.sh
@@ -37,7 +37,7 @@ pytest_bootstrap() {
 prepare() {
     export KUBECONFIG="${HOME}/.kube/kind-config-pytest"
     # Create kind cluster
-    kind create cluster --name=pytest --wait 300s
+    kind create cluster --name=pytest --wait 300s -q
     # Wait while all components in kube-system namespace will start
     kubectl wait --for=condition=Ready pod --all -n kube-system --timeout=300s
     kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
