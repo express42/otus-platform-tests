ad51d74cd4e0c6c9f907c0d767b2da7f31717380
Jasstkn <jasssstkn@yahoo.com>
2021-01-04 23:37:49 +0300

    Fix var

--- run.sh
+++ run.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 GROUP=github-actions
-BRANCH=${GITHUB_REF}
+BRANCH=$GITHUB_REF
 HOMEWORK_RUN=./otus-platform-tests/homeworks/$BRANCH/run.sh
 REPO=https://github.com/express42/otus-platform-tests.git
 DOCKER_IMAGE=express42/otus-homeworks
 
 echo GROUP:$GROUP
+echo BRANCH:$BRANCH
 
 if [ "$BRANCH" == "" ]; then
 	echo "We don't have tests for master branch"
 	exit 0
 fi
 
-echo HOMEWORK:$BRANCH
-
 echo "Clone repository with tests"
-git clone -b $GROUP --single-branch $REPO
+git clone -b $GROUP --single-branch $REPO -v
 
 if [ -f $HOMEWORK_RUN ]; then
 	echo "Run tests"
