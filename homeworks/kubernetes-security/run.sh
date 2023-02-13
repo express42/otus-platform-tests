998c23796ca96cf510f3929d532ec2a3061b2db8
Konstantinov Oleg <41775158+Konstantinov86@users.noreply.github.com>
2023-02-13 22:10:57 +0200

    Update run.sh

--- homeworks/kubernetes-security/run.sh
+++ homeworks/kubernetes-security/run.sh
@@ -7,7 +7,7 @@ chmod +x kubectl
 sudo mv kubectl /usr/local/bin/
 
 # Download kind
-curl -Lo kind https://github.com/kubernetes-sigs/kind/releases/download/v0.4.0/kind-linux-amd64
+curl -Lo kind https://github.com/kubernetes-sigs/kind/releases/download/v0.17.0/kind-linux-amd64
 chmod +x kind
 sudo mv kind /usr/local/bin/
 
